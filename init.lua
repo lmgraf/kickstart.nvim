ddf29b99ad86ab39d86ab5a6c9c45cc2b034584e
Lukas Graf <graflukas218@gmail.com>
2025-12-26 18:40:53 +0100

    fix: use flat eslint config

--- init.lua
+++ init.lua
@@ -755,6 +755,16 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {}
 
+        eslint = {
+          settings = {
+            --helps eslint find the eslintrc when it's placed in a subfolder
+            workingDirectory = { mode = 'auto' },
+            experimental = {
+              useFlatConfig = true,
+            },
+          },
+        },
+
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -798,6 +808,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'eslint-lsp',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 

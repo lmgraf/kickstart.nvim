22561111be2f3002deb7a761ca935f22504afa10
Lukas Graf <graflukas218@gmail.com>
2026-01-22 09:27:05 +0100

    fix: not starting guess-indent.nvim

--- init.lua
+++ init.lua
@@ -254,19 +254,13 @@ local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  { -- Detect tabstop and shiftwidth automatically
+    'NMAC427/guess-indent.nvim',
+    opts = {
+      auto_cmd = true,
+    },
+  },
 
   {
     'tpope/vim-fugitive',

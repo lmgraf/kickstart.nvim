4cd45078fe30632790326b083c30723ce6861a03
Lukas Graf <graflukas218@gmail.com>
2026-02-06 13:50:37 +0100

    chore: specify plugin path for dev mode

--- init.lua
+++ init.lua
@@ -636,6 +636,9 @@ require('lazy').setup({
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope! In normal mode type `<space>sh` then write `lazy.nvim-plugin`
 }, {
+  dev = {
+    path = '~/lmgraf/nvim/',
+  },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table

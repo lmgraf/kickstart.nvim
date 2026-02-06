b6a7927c9b9295c2ccb6abd2868500687e46ce0b
Lukas Graf <graflukas218@gmail.com>
2026-02-06 16:14:29 +0100

    feat: add language servers

--- init.lua
+++ init.lua
@@ -327,16 +327,27 @@ require('lazy').setup({
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --  See `:help lsp-config` for information about keys and how to configure
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        clangd = {},
+        eslint = {
+          settings = {
+            -- helps eslint find the eslintrc when it's placed in a subfolder
+            workingDirectory = { mode = 'auto' },
+            experimental = {
+              useFlatConfig = true,
+            },
+          },
+        },
+        solargraph = {
+          settings = {
+            solargraph = {
+              diagnostics = true,
+              formatting = true,
+              completion = true,
+              rename = true,
+            },
+          },
+        },
+        tailwindcss = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -348,8 +359,11 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'lua-language-server', -- Lua Language server
-        'stylua', -- Used to format Lua code
+        'clang-format',
+        'lua-language-server',
+        'prettierd',
+        'pylsp',
+        'stylua',
         -- You can add other tools here that you want Mason to install
       })
 
